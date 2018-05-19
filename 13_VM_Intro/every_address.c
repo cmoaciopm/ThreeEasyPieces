ec0d48d1d2ebb23019f2f25af18cd3df3992e624
gonganmin <cmoaciopm@gmail.com>
2018-05-20 06:36:02 +0800

    Add test code for chapter 13

---
+++ 13_VM_Intro/every_address.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("location of code: %p\n", main);
+    printf("location of head: %p\n", malloc(1));
+    int x = 3;
+    printf("location of stack: %p\n", &x);
+}
